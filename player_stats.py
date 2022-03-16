f70a7d45ab0a338a4b1dc5a8247c8914fbfdaf4e
LinkBetweenWorlds <william@Davids-MacBook-Pro.local>
2022-03-15 21:36:07 -0700

    Updated Player Stats

--- player_stats.py
+++ player_stats.py
@@ -1,9 +1,15 @@
 name = ""
+#Player Level
 level = 1
 exp = 0
+#Player Items
 wood = 0
 stone = 0
-ore = 0
-metal = 0
+iron_ore = 0
+gold_ore = 0
+iron = 0
+gold = 0
+money = 0
 weapon = "Fists"
+weapon_inventory = ["Fists"]
 inventory = []
